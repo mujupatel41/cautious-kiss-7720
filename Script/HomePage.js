e9434de0a4af7fd7bd944b9b014419ea3780d03c
mujupatel <mujupatel41@gmail.com>
2022-12-16 16:20:58 +0530

    Day 4 Work Done

---
+++ Script/HomePage.js
@@ -0,0 +1,60 @@
+document.querySelector("#nav>div>h1").addEventListener("click",()=>{
+    window.location.reload();
+})
+document.getElementById("Sign").addEventListener("click",()=>{
+    window.location.href = "./SignUp.html"
+})
+document.getElementById("login").addEventListener("click",()=>{
+    window.location.href = "./Login.html"
+})
+document.getElementById("Demo").addEventListener("click",()=>{
+    window.location.href = "SignUp.html"
+})
+document.getElementById("Demo-2").addEventListener("click",()=>{
+    window.location.href = "SignUp.html"
+})
+document.querySelector("#email+button").addEventListener("click",()=>{
+    let data = JSON.parse(localStorage.getItem("userdata")) || [];
+    let email = document.getElementById("email").value;
+
+    let obj = {
+        email,
+    }
+    data.push(obj)
+    localStorage.setItem("userdata",JSON.stringify(data))
+    alert("SignUp Sucsessful!")
+    window.location.href = "./LogIn.html"
+})
+document.querySelector("#nav>:nth-child(1)>:nth-child(2)").addEventListener("mouseover",()=>{
+    document.getElementById("Block0").style.display = "inline"
+    document.getElementById("Block0").style.display = "grid";
+    document.getElementById("Block0").style.paddingBottom = "230px"
+    document.getElementById("Block0").style.gridtemplatecolumns = "repeat(5,1fr)";
+    document.getElementById("Block1").style.display = "none"
+})
+document.querySelector("#nav>:nth-child(1)>:nth-child(2)").addEventListener("mouseout",()=>{
+    document.getElementById("Block0").style.display = "none"
+    document.getElementById("Block1").style.display = "flex"
+})
+document.querySelector("#nav>:nth-child(1)>:nth-child(4)").addEventListener("mouseover",()=>{
+    document.getElementById("Block0").style.display = "inline"
+    document.getElementById("Block0").style.display = "grid";
+    document.getElementById("Block0").style.paddingBottom = "230px"
+    document.getElementById("Block0").style.gridtemplatecolumns = "repeat(5,1fr)";
+    document.getElementById("Block1").style.display = "none"
+})
+document.querySelector("#nav>:nth-child(1)>:nth-child(4)").addEventListener("mouseout",()=>{
+    document.getElementById("Block0").style.display = "none"
+    document.getElementById("Block1").style.display = "flex"
+})
+document.querySelector("#nav>:nth-child(1)>:nth-child(6)").addEventListener("mouseover",()=>{
+    document.getElementById("Block0").style.display = "inline"
+    document.getElementById("Block0").style.display = "grid";
+    document.getElementById("Block0").style.paddingBottom = "230px"
+    document.getElementById("Block0").style.gridtemplatecolumns = "repeat(5,1fr)";
+    document.getElementById("Block1").style.display = "none"
+})
+document.querySelector("#nav>:nth-child(1)>:nth-child(6)").addEventListener("mouseout",()=>{
+    document.getElementById("Block0").style.display = "none"
+    document.getElementById("Block1").style.display = "flex"
+})
